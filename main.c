132dce44aa4cfaa4f6baa89edcc30c0b42c0bfaa
Unknown <albertopereira7@gmail.com>
2018-11-16 19:13:31 +0100

    Upload example file, New dispFileInfo function

--- main.c
+++ main.c
@@ -2,19 +2,42 @@
 #include <stdlib.h>
 #include "sndfile.h"
 
+int dispFileInfo(SF_INFO* pInFileInfo);
+
 int main(void)
 {
   char *inFileName;
   SNDFILE *inFile;
   SF_INFO inFileInfo;
-  int fs;
 
   inFileName = "noise.wav";
 
   inFile = sf_open(inFileName, SFM_READ, &inFileInfo);
+
+  if(inFile == NULL)
+    {
+      printf("ERROR: file doesn't exist.\n");
+      return 1;
+    }
+
+  dispFileInfo(&inFileInfo);
+
   sf_close(inFile);
 
-  fs = inFileInfo.samplerate;
+  return 0;
+}
+
+int dispFileInfo(SF_INFO* pInFileInfo)
+{
+  int fs;
+
+  if(pInFileInfo == NULL)
+  {
+    printf("ERROR: SF_INFO pointer.\n");
+    return 1;
+  }
+
+  fs = pInFileInfo->samplerate;
   printf("Sample Rate = %d Hz\n", fs);
 
   return 0;
